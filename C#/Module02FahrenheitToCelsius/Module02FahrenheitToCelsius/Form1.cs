9a1a41085e08a6d837b4345ba3f12ee0468f2ca1
Cade Snedden <csnedden982@gmail.com>
2018-08-26 13:58:28 -0500

    Fahrenheit To Celsius
    Form that converts Fahrenheit to Celsius. Has a Close, Clear/Reset and Convert button. Input value for Fahrenheit, the Celsius value will display next to the Celsius lable on the form.

---
+++ C#/Module02FahrenheitToCelsius/Module02FahrenheitToCelsius/Form1.cs
@@ -0,0 +1,48 @@
+﻿//Cade Snedden
+//Advanced C#
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module02FahrenheitToCelsius
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /* Button to exit program enviroment */
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        /* Converts Fahrenheit to Celsius */
+        private void btnConFah_Click(object sender, EventArgs e)
+        {
+            string fah = txtBoxFah.Text;
+            int f;
+            bool conF = Int32.TryParse(fah, out f);
+
+            float conversion = ((f - 32) * 5/9);
+
+            lblCel.Text = conversion.ToString();
+
+        }
+
+        /* Button to reset the Fahrenheit value */
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtBoxFah.Text = "0";
+        }
+    }
+}
