806b41139062086cd59da5250f08285faf547bbb
Cade Snedden <csnedden982@gmail.com>
2018-08-26 11:59:35 -0500

    Conversion for Miles to Kilometers (vice versa)
    This shows three button clicks (Close, ConMiles and ConKilo). Close exits the program enviroment. ConMiles converts the Miles textbox value into Kilometers. ConKilo converts the Kilometers textbox value into miles.

---
+++ C#/Module02MilesToKilometers/Module02MilesToKilometers/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace Module02MilesToKilometers
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /* btnClose to exit program enviroment */
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnConMiles_Click(object sender, EventArgs e)
+        {
+            const double conversion = 1.6;
+            double miles = Double.Parse(txtBoxMiles.Text);
+            double kilometers = Double.Parse(txtBoxKilometers.Text);
+
+            txtBoxKilometers.Text = (miles * conversion).ToString();
+
+        }
+
+        private void btnConKilo_Click(object sender, EventArgs e)
+        {
+            const double conversion = 1.6;
+            double miles = Double.Parse(txtBoxMiles.Text);
+            double kilometers = Double.Parse(txtBoxKilometers.Text);
+
+            txtBoxMiles.Text = (kilometers / conversion).ToString();
+        }
+    }
+}
