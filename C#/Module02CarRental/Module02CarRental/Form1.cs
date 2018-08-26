4018e261c91c81478787f8217fb053b66f87182f
Cade Snedden <csnedden982@gmail.com>
2018-08-26 12:28:22 -0500

    CarRental
    This shows a button click (btnTotal) that two textboxes' values (days and miles) and then multiplies those values by the corresponding const variable. It then spits the result to a label, displaying the total value on the form.

---
+++ C#/Module02CarRental/Module02CarRental/Form1.cs
@@ -0,0 +1,39 @@
+﻿//Cade Snedden
+//Advanced C#
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Module02CarRental
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+            const int dailyCharge = 20;
+            const double mileCharge = 0.25;
+            int days = int.Parse(txtBoxDays.Text);
+            double miles = Double.Parse(txtBoxMiles.Text);
+
+            lblTotal.Text = ((days * dailyCharge) + (miles * mileCharge)).ToString();
+            
+        }
+    }
+}
